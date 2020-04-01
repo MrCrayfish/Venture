ec2d024a3f2c06cf70177e21df7e1deb022a2626
MrCrayfish <mrcrayfish@hotmail.com>
2020-04-01 18:54:36 +1030

    Setup basic mod structure

---
+++ src/main/java/com/mrcrayfish/venture/Venture.java
@@ -0,0 +1,15 @@
+package com.mrcrayfish.venture;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod(Reference.MOD_ID)
+public class Venture
+{
+    public Venture()
+    {
+        
+    }
+}
