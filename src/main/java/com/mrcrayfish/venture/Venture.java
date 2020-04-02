1294daceeba3673f681445d8aa1e6d3749d97f1f
MrCrayfish <mrcrayfish@hotmail.com>
2020-04-02 20:05:18 +1030

    Huge ore now spawns into world

--- src/main/java/com/mrcrayfish/venture/Venture.java
+++ src/main/java/com/mrcrayfish/venture/Venture.java
@@ -1,19 +1,15 @@
 package com.mrcrayfish.venture;
 
-import com.google.common.collect.ImmutableList;
 import com.mrcrayfish.venture.init.ModFeatures;
 import net.minecraft.block.Blocks;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
-import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
-import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.placement.ChanceConfig;
-import net.minecraft.world.gen.placement.FrequencyConfig;
-import net.minecraft.world.gen.placement.IPlacementConfig;
+import net.minecraft.world.gen.placement.DepthAverageConfig;
 import net.minecraft.world.gen.placement.Placement;
-import net.minecraft.world.gen.treedecorator.LeaveVineTreeDecorator;
-import net.minecraft.world.gen.treedecorator.TrunkVineTreeDecorator;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -25,8 +21,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(Reference.MOD_ID)
 public class Venture
 {
-    public static final HugeTreeFeatureConfig HUGE_TREE_CONFIG = (new HugeTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.JUNGLE_LOG.getDefaultState()), new SimpleBlockStateProvider(Blocks.JUNGLE_LEAVES.getDefaultState()))).baseHeight(10).func_227283_b_(20).func_227282_a_(ImmutableList.of(new TrunkVineTreeDecorator(), new LeaveVineTreeDecorator())).setSapling((net.minecraftforge.common.IPlantable) Blocks.JUNGLE_SAPLING).build();
-
     public Venture()
     {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -39,6 +33,6 @@ public class Venture
         Biomes.PLAINS.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ModFeatures.SURVIVAL_CAMP.get().func_225566_b_(IFeatureConfig.NO_FEATURE_CONFIG).func_227228_a_(Placement.CHANCE_TOP_SOLID_HEIGHTMAP.func_227446_a_(new ChanceConfig(40))));
         Biomes.SUNFLOWER_PLAINS.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ModFeatures.SURVIVAL_CAMP.get().func_225566_b_(IFeatureConfig.NO_FEATURE_CONFIG).func_227228_a_(Placement.CHANCE_TOP_SOLID_HEIGHTMAP.func_227446_a_(new ChanceConfig(40))));
         Biomes.SAVANNA.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ModFeatures.SURVIVAL_CAMP.get().func_225566_b_(IFeatureConfig.NO_FEATURE_CONFIG).func_227228_a_(Placement.CHANCE_TOP_SOLID_HEIGHTMAP.func_227446_a_(new ChanceConfig(40))));
-        //.func_227228_a_(Placement.NOPE.func_227446_a_(IPlacementConfig.NO_PLACEMENT_CONFIG))
+        Biome.BIOMES.forEach(biome -> biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, ModFeatures.HUGE_ORE.get().func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.IRON_ORE.getDefaultState(), 50)).func_227228_a_(Placement.COUNT_DEPTH_AVERAGE.func_227446_a_(new DepthAverageConfig(1, 24, 4)))));
     }
 }
--- src/main/java/com/mrcrayfish/venture/init/ModFeatures.java
+++ src/main/java/com/mrcrayfish/venture/init/ModFeatures.java
@@ -1,9 +1,11 @@
 package com.mrcrayfish.venture.init;
 
 import com.mrcrayfish.venture.Reference;
+import com.mrcrayfish.venture.world.gen.feature.HugeOreFeature;
 import com.mrcrayfish.venture.world.gen.feature.SurvivalCampFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,4 +18,5 @@ public class ModFeatures
     public static final DeferredRegister<Feature<?>> REGISTER = new DeferredRegister<>(ForgeRegistries.FEATURES, Reference.MOD_ID);
 
     public static final RegistryObject<SurvivalCampFeature> SURVIVAL_CAMP = REGISTER.register("survival_camp", () -> new SurvivalCampFeature(NoFeatureConfig::deserialize));
+    public static final RegistryObject<HugeOreFeature> HUGE_ORE = REGISTER.register("huge_ore", () -> new HugeOreFeature(OreFeatureConfig::deserialize));
 }
